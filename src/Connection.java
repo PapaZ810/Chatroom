d9a53a50e7a9737022ede53273959c5546d6fbc2
Zac Durham <43076672+PapaZ810@users.noreply.github.com>
2023-11-14 10:34:08 -0700

    Initial commit

---
+++ src/Connection.java
@@ -0,0 +1,23 @@
+import java.net.*;
+
+public class Connection implements Runnable
+{
+	private Socket	client;
+	private static Handler handler = new Handler();
+
+	public Connection(Socket client) {
+		this.client = client;
+	}
+
+	/**
+	 * This method runs in a separate thread.
+	 */
+	public void run() { 
+		try {
+			handler.process(client);
+		}
+		catch (java.io.IOException ioe) {
+			System.err.println(ioe);
+		}
+	}
+}
